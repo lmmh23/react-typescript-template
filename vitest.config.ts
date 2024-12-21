e294c0d08b24e09a026ac4538e98a42c2da8096c
minhyeong <peterlee11@naver.com>
2024-12-22 01:09:47 +0900

    feat: Apply vitest setup absolute path

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
 import packageJson from "./package.json"
+import { resolve } from "path"
 import { defineConfig } from "vitest/config"
 
 export default defineConfig({
@@ -7,5 +8,8 @@ export default defineConfig({
     dir: "./src",
     environment: "jsdom",
     setupFiles: "./vitest.setup.ts",
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
   },
 })
