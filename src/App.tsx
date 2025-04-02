01cd19ca8cf6f38672754411b01a819fe9824d06
Lee <peterlee11@naver.com>
2025-04-02 10:40:26 +0900

    feat: styled-components -> tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import "./App.css"
 
 function App() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
+  return <h1 className='text-2xl text-amber-200'>Hello World!</h1>
 }
 
 export default App
