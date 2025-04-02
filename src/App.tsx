cff3c1280316ac37967c4390ffd22b2155feffde
Lee <peterlee11@naver.com>
2025-04-02 10:51:30 +0900

    chore: tailwind prettier plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css"
 
 function App() {
-  return <h1 className='text-2xl text-amber-200'>Hello World!</h1>
+  return <h1 className="border text-2xl text-amber-200">Hello World!</h1>
 }
 
 export default App
